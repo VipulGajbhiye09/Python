c46fd3c885f7af8a54e7a413fbe5f6216f36fcc5
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-20 23:58:40 +0530

    Update basics01.py

--- basics01.py
+++ basics01.py
@@ -7,3 +7,15 @@ b=3.14     #float
 c="abc"    #string 
 
 d=True     #boolean 
+
+'''
+Q] Primitive data types in python?
+
+1)int 2)float 3)str 4)bool 5)complex
+
+'''
+num = input("Enter a number")  #prompt message will be displayed on terminal
+'''
+default mode of input is string
+'''
+type(num) #string
