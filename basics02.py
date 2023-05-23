3f8aeb7038ca372e6913f097c715f1f4b1b9ff96
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-23 23:26:07 +0530

    Update basics02.py

--- basics02.py
+++ basics02.py
@@ -3,7 +3,18 @@ Bitwise operators
 1) ~ (Two's Complement)  
   To calculate Two's Complement there are two steps 1] reverse the sign 2] substract 1
   E.g.) ~125 = -125 -1 = -126
+  
+2) << (Left Shift)  
+  To calculate Left Shift multiply a by 2 raised to n
+  a << n = a * (2^n)
+  
+   E.g.) 20<<2 gives 80
 
 
+3) >> (Right Shift)
+  To calculate Right Shift floor divide a by 2 raised to n
+  a >> n = a // (2^n)
+  
+   E.g.) 100 >> 2 gives 25
 
 '''
