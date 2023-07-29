0f527a48722cdb507967d19ae6fc4b239244bd39
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-07-29 22:00:30 +0530

    Update queue_01.py

--- queue_01.py
+++ queue_01.py
@@ -7,3 +7,13 @@ class QueueExample:
         for i in range(self.MaxSize):
             self.queue.append(0)
         print("Queue of size",self.MaxSize,"created")
+
+    def enqueue(self,e):
+        self.rear+=1 #rear=rear+1
+        self.queue[self.rear]=e
+        print("Enqueued",e)
+    def isFull(self):
+        if self.rear==self.MaxSize-1:
+            return True
+        else:
+            return False
