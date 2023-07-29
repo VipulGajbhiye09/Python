aa875c814777214b93f56ea594a6df05888c10da
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-07-29 22:01:08 +0530

    Update queue_01.py

--- queue_01.py
+++ queue_01.py
@@ -12,8 +12,24 @@ class QueueExample:
         self.rear+=1 #rear=rear+1
         self.queue[self.rear]=e
         print("Enqueued",e)
+        
     def isFull(self):
         if self.rear==self.MaxSize-1:
             return True
         else:
             return False
+
+    def dequeue(self):
+        temp=self.queue[self.front]
+        self.front+=1
+        return temp
+
+    def isEmpty(self):
+        if self.front>self.rear:
+            return True
+        else:
+            return False
+            
+    def printQueue(self):
+        for i in range(self.front,self.rear+1,+1):
+            print(self.queue[i],"--",end=" ")
