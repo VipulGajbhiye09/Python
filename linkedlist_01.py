928420c9f1a2f212988e5de5c48bdcfb7abfc286
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-07-30 23:05:37 +0530

    Update linkedlist_01.py

--- linkedlist_01.py
+++ linkedlist_01.py
@@ -6,3 +6,20 @@ class Node:
 class LinkedList:
     def createList(self):
         self.root=None
+        
+    def insert_left(self,data):
+        n=Node(data)   #Node Creation
+        if self.root==None:
+            self.root=n
+        else:
+            n.next=self.root  #1
+            self.root=n  #2
+        print(data,"inserted")
+
+    def delete_left(self):
+        if self.root==None:
+            print("List Empty")
+        else:
+            t=self.root  #1
+            self.root=self.root.next  #2
+            print(t.data,"deleted")
