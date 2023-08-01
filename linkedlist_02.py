bb6c58a8d593fa8da7ba7b4fac1a0069b677aad3
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-01 22:25:37 +0530

    Update linkedlist_02.py

--- linkedlist_02.py
+++ linkedlist_02.py
@@ -5,12 +5,24 @@ class CircularLinkedList:
         self.root=self.last=None
 
     def insert_left(self,data):
-        n=Node(data)#Node Creation
+        n=Node(data)   #Node Creation
         if self.root==None:
             self.root=self.last=n
             self.last.next=self.root
         else:
-            n.next=self.root#1
-            self.root=n#2
+            n.next=self.root  #1
+            self.root=n  #2
             self.last.next=self.root#3
         print(data,"inserted")
+        
+    def delete_left(self):
+        if self.root==None:
+            print("List Empty")
+        else:
+            t=self.root  #1
+            if self.root==self.last:  #one element
+                self.root=self.last=None
+            else:
+                self.root=self.root.next  #2
+                self.last.next=self.root  #3
+            print(t.data,"deleted")
