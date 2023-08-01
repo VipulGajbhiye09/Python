74e5e9c80564db17d96ddbe376f5e891e4814960
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-01 22:26:41 +0530

    Update linkedlist_02.py

--- linkedlist_02.py
+++ linkedlist_02.py
@@ -37,3 +37,29 @@ class CircularLinkedList:
             self.last=n  #2
             self.last.next=self.root  #3
         print(data,"inserted")
+        
+    def delete_right(self):
+        if self.root==None:
+            print("List Empty")
+        else:
+            t=t2=self.root  #1
+            if self.root==self.last:  #one element
+                self.root=self.last=None
+            else:
+                while t!=self.last:  #2
+                    t2=t
+                    t=t.next
+                self.last=t2  #3
+                self.last.next=self.root  #4
+            print(t.data,"deleted")
+
+    def print_list(self):
+        if self.root==None:
+            print("Empty List")
+        else:
+            t=self.root
+            while True:
+                print(t.data,end="-->")
+                t=t.next
+                if t==self.root:
+                    break
