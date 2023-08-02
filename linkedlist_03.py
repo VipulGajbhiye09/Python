c789a83a63d2fdd436f9fff3825419f066b6a95a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-02 22:50:00 +0530

    Update linkedlist_03.py

--- linkedlist_03.py
+++ linkedlist_03.py
@@ -11,3 +11,12 @@ class DoublyLinkedList:
             self.root.left=n  #2
             self.root=n  #3
         print(data,"inserted")
+
+    def delete_left(self):
+        if self.root==None:
+            print("List Empty")
+        else:
+            t=self.root  #1
+            self.root=self.root.right  #2
+            self.root.left=None#3
+            print(t.data,"deleted")
