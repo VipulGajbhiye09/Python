5664d7235dcada7f53c6f448036340552e3a1137
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-05 23:40:17 +0530

    Update stack_02.py

--- stack_02.py
+++ stack_02.py
@@ -36,3 +36,23 @@ class StackExample:
             while t!=None:
                 print(t.data)
                 t=t.next
+
+## Menu Driven Code
+
+obj=StackExample()
+obj.createstack()
+while True:
+    ch=int(input("1.Push\n2.Pop\n3.Peek\n4.Print\n0.Exit\n:"))
+    if ch==1:
+        obj.push(int(input("Enter the element:")))
+    elif ch==2:
+        obj.pop()
+    elif ch==3:
+      obj.peek()
+    elif ch==4:
+        obj.print_stack()
+    elif ch == 0:
+      print("Thanks")
+      break
+    else:
+        print("Wrong option selected")
