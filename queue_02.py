8c5302718d5b50f8da6809650014c36acabe6c97
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-09 22:15:00 +0530

    Update queue_02.py

--- queue_02.py
+++ queue_02.py
@@ -1,7 +1,18 @@
+## Queue implementation with Linked List
+
 class QueueExample:
     def createqueue(self):
         self.front=self.rear=None
 
+    def enqueue(self,data):
+            n=Node(data)
+            if self.front==None:
+                self.front=self.rear=n
+            else:
+                self.rear.next=n  #3
+                self.rear=n
+            print(data,"enqueued")
+        
     def dequeue(self):
         if self.front==None:
             print("Queue Empty")
@@ -9,3 +20,5 @@ class QueueExample:
             t=self.front  #1
             self.front=self.front.next  #2
             print(t.data,"dequeued")
+
+    
