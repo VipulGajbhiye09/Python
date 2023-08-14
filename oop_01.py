0bd85772511edfe3f9dc9baebc4a1b4ff8cec175
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-14 23:10:53 +0530

    Update oop_01.py

--- oop_01.py
+++ oop_01.py
@@ -5,8 +5,18 @@ class Human:
         self.gender=input("Enter gender:")
         self.name=input("Enter name:")
     def intro(self):
-        print(f"hi i am a"{self.gender} named {self.name}")
+        print(f"Hi I'm a {self.gender} named {self.name}")
 
 obj = Human()  ##object
 obj.birth()
 obj.intro()
+
+##Constructor is a special method that is automatically invoked when object of that class is created
+##Constructor is used to initialize an object
+
+class Human:
+    def __init__(self,name,gender):  ##Constructor
+        print("init called:")
+        self.gender=gender
+        self.name=name
+        print(f"Hi I'm a {self.gender} named {self.name}")
